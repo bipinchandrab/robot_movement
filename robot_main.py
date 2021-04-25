e30be1af8b81e0b3159416df4e3c75402079364e
bipinchandrab <79129910+bipinchandrab@users.noreply.github.com>
2021-04-25 12:16:50 +1000

    Just initiating project. Just adding input methods in desired format and checking results.

---
+++ robot_main.py
@@ -0,0 +1,9 @@
+import re
+#importing RegEx to take inputs in desired format
+directions = []
+final_position = []
+placing, x, y, z = list(re.split(r'[, ]', input()))
+
+
+if __name__ == "__main__":
+    print({placing})

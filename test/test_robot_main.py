d621066f57735dfd2106ba6823c50ea489fab092
bipin chandra <bipin.b.chandra@gmail.com>
2021-04-25 15:06:43 +1000

    Testing the user_input with unit tests

--- test/test_robot_main.py
+++ test/test_robot_main.py
@@ -1,9 +1,12 @@
 import unittest
 
+from robot_main import *
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_input(self):
+        self.assertEqual(placing, "PLACE")
+        assert True
 
 
 if __name__ == '__main__':

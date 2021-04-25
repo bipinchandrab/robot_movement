a8827e2c12ee7cb8e1542e2403ee150300157e32
bipin chandra <bipin.b.chandra@gmail.com>
2021-04-25 17:34:43 +1000

    Adding Test Data

--- test/test_robot_main.py
+++ test/test_robot_main.py
@@ -6,7 +6,9 @@ from robot_main import *
 class MyTestCase(unittest.TestCase):
     def test_input(self):
         self.assertEqual(placing, "PLACE")
-        assert True
+        self.assertEqual(x_coordinate, 0 or 1 or 2 or 3 or 4)
+        self.assertEqual(y_coordinate,0 or 1 or 2 or 3 or 4)
+        self.assertEqual(z,'NORTH' or 'SOUTH' or 'EAST' or 'WEST')
 
 
 if __name__ == '__main__':
